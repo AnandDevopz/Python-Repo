d78b14c02570323da9a1566603eedeaf4fac9556
AnandDevopz <134300796+AnandDevopz@users.noreply.github.com>
2023-05-22 21:01:00 +0530

    Create input_example.py

---
+++ input_example.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 22 20:49:59 2023
+
+@author: saumyabm
+"""
+
+a=int(input("Enter any number :"))
+b=int(input("Enter another number:"))
+print(type(a))
+print(type(b))
+print(a+b)
